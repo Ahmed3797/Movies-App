579f8b58b9ea491915ba592f9b51f46d8ad39e43
Ahmed Siddique <ch379796@gmail.com>
2024-07-08 18:14:56 +0500

    "After some Formatting"

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar, StyleSheet } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
-import MovieScreen from './screens/MovieScreen';
-import SearchScreen from './screens/SearchScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import MovieScreen from "./screens/MovieScreen";
+import SearchScreen from "./screens/SearchScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,31 +10,32 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#1B1122', // Custom header background color
-        },
-        headerTintColor: '#fff', // Custom header text color
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
-      
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#1B1122",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
       >
-        <Stack.Screen name="Home" component={HomeScreen} 
-        options={{headerTitle:"MOVIE APP"}}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerTitle: "MOVIE APP" }}
         />
-        <Stack.Screen name="Movies" component={MovieScreen} 
-
-        options={{headerShown:false}}
-
+        <Stack.Screen
+          name="Movies"
+          component={MovieScreen}
+          options={{ headerShown: false }}
         />
-        <Stack.Screen name="Search" component={SearchScreen}
-        options={{headerTitle:"Search"}}
-         />
+        <Stack.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{ headerTitle: "Search" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
